1a5bd7155ebe9803e158ceda32269c9b8ce86050
garfieldno1 <75411444+garfieldno1@users.noreply.github.com>
2025-11-10 04:53:13 +0000

    Added the testcase

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,20 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(2, 2), 4)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(1, 2), 2)
+        self.assertEqual(self.calc.multiply(2, 2), 4)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(13, 2), 1)
+       
 
     # Add the following test methods to the TestCalculator class:
 
